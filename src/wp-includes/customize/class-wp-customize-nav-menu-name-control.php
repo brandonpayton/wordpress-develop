d678dfe4d0c263fa14a9c8348fb436d1318abd5f
Brandon Payton <brandon@happycode.net>
2017-10-03 12:50:14 -0700

    Show description in nav menu name field

--- src/wp-includes/customize/class-wp-customize-nav-menu-name-control.php
+++ src/wp-includes/customize/class-wp-customize-nav-menu-name-control.php
@@ -42,7 +42,14 @@ class WP_Customize_Nav_Menu_Name_Control extends WP_Customize_Control {
 			<# if ( data.label ) { #>
 				<span class="customize-control-title">{{ data.label }}</span>
 			<# } #>
-			<input type="text" class="menu-name-field live-update-section-title" />
+			<input type="text" class="menu-name-field live-update-section-title"
+				<# if ( data.description ) { #>
+					aria-describedby="{{ data.section }}-description"
+				<# } #>
+				/>
+			<# if ( data.description ) { #>
+				<p id="{{ data.section }}-description">{{ data.description }}</p>
+			<# } #>
 		</label>
 		<?php
 	}
